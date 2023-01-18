7b5784af0c10db0184b6e587f2ab39a3e29a913f
mameaw14 <pinyapornster@gmail.com>
2023-01-19 00:38:21 +0700

    Add jest

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://mameaw14.xyz',
   integrations: [mdx(), sitemap()],
 })
