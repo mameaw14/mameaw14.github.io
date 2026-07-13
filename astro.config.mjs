031d425359be594a533972ebc9db3f63fdfc35a6
mameaw14 <pinyapornster@gmail.com>
2026-07-13 23:56:49 +0700

    fix: tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
 import react from '@astrojs/react'
-
-const tailwindConfig = {}
 
 export default defineConfig({
 	site: 'https://mameaw14.xyz',
-	integrations: [mdx(), sitemap(), tailwind(tailwindConfig), react()],
+	integrations: [mdx(), sitemap(), react()],
 	markdown: {
 		shikiConfig: {
 			theme: 'dracula-soft',
